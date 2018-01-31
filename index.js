81ea7b75c9c1896fa9b13bec9654586cb6c42317
carlitos321 <ca.cabrera321@gmail.com>
2018-01-31 19:40:55 +0000

    Done.

--- index.js
+++ index.js
@@ -6,18 +6,28 @@ function nestedTarget() {
   return document.querySelector('#nested .target')
 }
 
-function increaseRankBy(n) {
-  const allLi = document.querySelectorAll('ul.ranked-list li').innerHTML
+function deepestChild() {
+  let node = document.querySelector('#grand-node')
+  //above tells me to get the div
+  let nextNode = node.children[0]
+  //above tells me to get the div's first child
 
-  for (let i = 0; i < allLi.length; i++) {
-      parseInt(allLi[i] + n) 
+  while (nextNode) {
+    node = nextNode
+    nextNode = node.children[0]
   }
+
+  return node
 }
 
-function deepestChild() {
-  document.querySelector('#grand-node ')
+function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll('.ranked-list')
 
-  //select #grand-node parent
-  // Then drill to into the innerHTML of div
+  for (let i = 0, l = rankedLists.length; i < l; i++) {
+    let children = rankedLists[i].children
 
+    for (let j = 0, k = children.length; j < k; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n
+    }
+  }
 }
